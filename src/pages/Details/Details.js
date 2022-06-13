a4ad7f3276634b327a0a1b5c2b618b779755e349
Abdul <abdulmomin956@gmail.com>
2022-06-13 13:16:51 +0600

    details add

--- src/pages/Details/Details.js
+++ src/pages/Details/Details.js
@@ -5,16 +5,37 @@ const Details = ({ data, text }) => {
     const { id } = useParams();
 
 
-    const singleItem = data.filter(item => item.id === parseInt(id))
+    const singleItemArr = data.filter(item => item.id === parseInt(id))
+    const singleItem = singleItemArr[0]
     console.log(singleItem);
 
 
-
     return (
-        <div>
-            {id}
-
-        </div>
+        <section className='container'>
+            <div className='border my-3'>
+                <h1>Project Name: {singleItem.name}</h1>
+                <hr />
+                <p>{singleItem.feature}</p>
+            </div>
+            <div className='border'>
+                <h1>Project Info:</h1>
+                <hr />
+                <table>
+                    <tr>
+                        <td>Category:</td>
+                        <td>{singleItem.type}</td>
+                    </tr>
+                    <tr>
+                        <td>Date:</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>Website:</td>
+                        <td></td>
+                    </tr>
+                </table>
+            </div>
+        </section>
     );
 };
 
