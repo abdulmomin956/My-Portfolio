fd1efdb4ddac14d1a667760bbfeccfeda5d43c6d
Abdul Momin <66585914+abdulmomin956@users.noreply.github.com>
2024-12-11 22:50:39 +0600

    auto year update

--- src/pages/shared/Footer.js
+++ src/pages/shared/Footer.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 const Footer = () => {
+    const d = new Date();
+    let year = d.getFullYear();
     return (
         <div className='py-5 text-center container'>
             <hr />
-            <p>&#169; copyright 2020 | abdulmomin956</p>
+            <p>&#169; copyright {year} | abdulmomin956</p>
         </div>
     );
 };
